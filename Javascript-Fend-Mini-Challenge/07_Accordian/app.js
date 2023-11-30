3e7d4824081422ac7e8f88dcc3caf9de92c61994
Chandan Kumar <krc6893@gmail.com>
2023-11-30 11:53:32 +0530

    accordianJS doneTo close an open accordion when another one is clicked, you need to keep track of the currently open accordion and close it when a new one is clicked. Here's an updated version of your JavaScript code to achieve this

--- Javascript-Fend-Mini-Challenge/07_Accordian/app.js
+++ Javascript-Fend-Mini-Challenge/07_Accordian/app.js
@@ -1,15 +1,39 @@
-// youtube Link
-// https://www.youtube.com/watch?v=vU_U-UBMOnM
+// // youtube Link
+// // https://www.youtube.com/watch?v=vU_U-UBMOnM
+// const accordians = document.querySelectorAll(".accordian");
+
+// accordians.forEach((accordian) => {
+//   const icon = accordian.querySelector(".icon");
+//   const answer = accordian.querySelector(".answer");
+//   accordian.addEventListener("click", () => {
+//     // icon.classList.toggle("active");
+//     // answer.classList.toggle("active");
+
+//     // BUT best would be
+
+//     if (icon.classList.contains("active")) {
+//       icon.classList.remove("active");
+//       answer.style.maxHeight = null;
+//     } else {
+//       icon.classList.add("active");
+//       answer.style.maxHeight = answer.scrollHeight + "px";
+//     }
+//   });
+// });
+
 const accordians = document.querySelectorAll(".accordian");
+let currentAccordion = null;
 
 accordians.forEach((accordian) => {
   const icon = accordian.querySelector(".icon");
   const answer = accordian.querySelector(".answer");
   accordian.addEventListener("click", () => {
-    // icon.classList.toggle("active");
-    // answer.classList.toggle("active");
-
-    // BUT best would be
+    if (currentAccordion && currentAccordion !== accordian) {
+      const currentIcon = currentAccordion.querySelector(".icon");
+      const currentAnswer = currentAccordion.querySelector(".answer");
+      currentIcon.classList.remove("active");
+      currentAnswer.style.maxHeight = null;
+    }
 
     if (icon.classList.contains("active")) {
       icon.classList.remove("active");
@@ -18,5 +42,7 @@ accordians.forEach((accordian) => {
       icon.classList.add("active");
       answer.style.maxHeight = answer.scrollHeight + "px";
     }
+
+    currentAccordion = accordian;
   });
 });
